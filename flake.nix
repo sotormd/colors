3dfbaa93653d719fb8f937489d6f94b4dad32102
vrm <sotormd@proton.me>
2025-12-18 10:02:58 +0530

    refactor: cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,23 @@
 {
   description = "colorscheme flake";
 
-  outputs = { self, ... }@inputs: { lib.colors = import ./colors.nix; };
+  outputs =
+    _:
+    let
+      colors = import ./colors.nix;
+    in
+    {
+      lib.colors = colors;
+
+      nixosModules.colors =
+        { lib, ... }:
+        {
+          options.colors = lib.mkOption {
+            type = lib.types.attrs;
+            readOnly = true;
+            default = colors;
+            description = "Color schemes exposed by the colorscheme flake.";
+          };
+        };
+    };
 }
